15f1aa553414d8b2a6f6414ad950da0f4f8ddb0b
彭振 <kingkongpeng@163.com>
2024-10-18 11:06:14 +0800

    初始化项目

---
+++ ImageClassifierTests/ImageClassifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassifierTests.swift
+//  ImageClassifierTests
+//
+//  Created by 彭振 on 2024/10/18.
+//
+
+import Testing
+@testable import ImageClassifier
+
+struct ImageClassifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
